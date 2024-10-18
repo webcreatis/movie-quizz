a8f2b4f74e2c20d6dd81d749774a010c5cd090eb
webcreatis <contact@webcreatis.fr>
2024-10-18 10:16:54 +0400

    Quizz > Initialize Lenis

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 import "./style.css";
+import Lenis from "lenis";
+
+// Initialize Lenis
+const lenis = new Lenis();
+
+// Listen for the scroll event and log the event data
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
