e69413d5d75ad73aaaa9c24a6072c17af75d53b8
webcreatis <contact@webcreatis.fr>
2024-10-19 11:25:55 +0400

    Quizz > create pics wall + animation + responsive

---
+++ picsWall.js
@@ -0,0 +1,84 @@
+// Fonction pour charger le JSON
+fetch("./pics.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const imageGrid = document.getElementById("image-grid");
+
+    // Création d'un container pour chaque image et on l'ajoute au DOM
+    const imageBoxes = data.map((image, index) => {
+      const imageBox = document.createElement("div");
+      imageBox.classList.add(
+        "overflow-hidden",
+        "opacity-0",
+        "min-[320px]:w-[100px]",
+        "min-[320px]:h-[100px]",
+        "min-[768px]:w-[120px]",
+        "min-[768px]:h-[120px]",
+        "tablet:w-[200px]",
+        "tablet:h-[200px]",
+        "min-[1440px]:w-[250px]",
+        "min-[1440px]:h-[250px]",
+        "desktopHD:w-[300px]",
+        "desktopHD:h-[300px]"
+      );
+
+      // Ajout de l'image à la div
+      imageBox.innerHTML = `
+        <img src="${image.src}" alt="${image.alt}" class="w-full h-full object-cover transition-transform scale-100">
+      `;
+
+      // Ajout de l'image au container parent
+      imageGrid.appendChild(imageBox);
+
+      // Lance l'animation de chaque image avec un délai
+      setTimeout(() => {
+        imageBox.classList.remove("opacity-0");
+        imageBox.classList.add("opacity-100");
+      }, index * 100);
+
+      return imageBox;
+    });
+
+    // Sélectionne une image aléatoire pour une animation infinie
+    const startRandomInfiniteAnimation = () => {
+      let lastAnimatedImage = null;
+
+      setInterval(() => {
+        // Si une image est déjà animée, on retire l'animation et on supprime le flou des autres ainsi que l'effet zoom
+        if (lastAnimatedImage) {
+          lastAnimatedImage.classList.remove("z-20");
+          imageBoxes.forEach((imgBox) => {
+            imgBox.classList.remove("blur-md");
+            imgBox.classList.remove("scale-75");
+          });
+        }
+
+        // Sélectionne une image aléatoire
+        const randomIndex = Math.floor(Math.random() * imageBoxes.length);
+        const randomImage = imageBoxes[randomIndex];
+
+        // Ajout de l'animation à l'image sélectionnée
+        randomImage.classList.add(
+          "duration-100",
+          "animate-opacity-fade",
+          "scale-75"
+        );
+
+        // Ajout de l'effet de flou et zoom à toutes les autres images
+        imageBoxes.forEach((imgBox, idx) => {
+          if (idx !== randomIndex) {
+            imgBox.classList.add("blur-md", "scale-100");
+          }
+        });
+
+        // Sauvegarde l'image animée pour la retirer au prochain cycle
+        lastAnimatedImage = randomImage;
+      }, 1000);
+    };
+
+    // Lance l'animation infinie après que toutes les images sont affichées
+    setTimeout(startRandomInfiniteAnimation, data.length * 100);
+  })
+  .catch((error) =>
+    console.error("Erreur lors du chargement des images:", error)
+  );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      tablet: "640px",
+      laptop: "1024px",
+      desktopHD: "2560px",
+    },
+    extend: {
+      animation: {
+        "opacity-fade": "opacity 0.3s cubic-bezier(0.895, 0.03, 0.685, 0.22)",
+      },
+      keyframes: {
+        opacity: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      transitionTimingFunction: {
+        "custom-ease": "cubic-bezier(0.895, 0.03, 0.685, 0.22)",
+      },
+      colors: {
+        black: "#1D1D1D ",
+      },
+    },
   },
   plugins: [],
 };
