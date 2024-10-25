20c9440e59be9c99e24d8216ff2a04877b5fcf89
webcreatis <contact@webcreatis.fr>
2024-10-25 11:41:04 +0400

    QUIZZ > add questions + fix responsive + add display final score message

--- questions.js
+++ questions.js
@@ -43,4 +43,262 @@ export const Questions = [
     bg: "/freddy.webp",
     audioUrl: "/freddy.mp3",
   },
+  {
+    question: "De quel film d'horreur de 1976 est extraite cette image ?",
+    answers: ["Shining", "Hellraiser", "Carrie", "Dracula"],
+    correct: "Carrie",
+    bg: "/carrie.webp",
+    audioUrl: "/carrie.mp3",
+  },
+  {
+    question:
+      "Quel est le nom de ce personnage emblématique connu pour son visage recouvert de clous, qui est le chef des Cénobites ?",
+    answers: ["Chatterer", "Butterball", "Pinhead", "Leatherface"],
+    correct: "Pinhead",
+    bg: "/pinhead.webp",
+    audioUrl: "/hellraiser.mp3",
+  },
+  {
+    question:
+      "Dans Vendredi 13 (1980), quel est le nom du meurtrier qui tue les campeurs à Crystal Lake dans le premier film ?",
+    answers: [
+      "Tommy Jarvis",
+      "Elias Voorhees",
+      "Pamela Voorhees",
+      "Jason Voorhees",
+    ],
+    correct: "Pamela Voorhees",
+    bg: "/vendredi13.webp",
+    audioUrl: "/friday.mp3",
+  },
+  {
+    question:
+      "Où est assassinée Marion dans le film culte Psychose d’Alfred Hitchcock ?",
+    answers: [
+      "Dans sa cave",
+      "Dans sa douche",
+      "Dans sa voiture",
+      "Dans sa chambre",
+    ],
+    correct: "Dans sa douche",
+    bg: "/psycho.webp",
+    audioUrl: "/psycho.mp3",
+  },
+  {
+    question:
+      "Dans L'Exorciste, quel est le nom du démon qui possède la jeune fille Regan ?",
+    answers: ["Azazel", "Beelzebut", "Pazuzu", "Belial"],
+    correct: "Pazuzu",
+    bg: "/exorciste.webp",
+    audioUrl: "/exorciste.mp3",
+  },
+  {
+    question:
+      "Dans The Fog (1980), quel est le nom du navire légendaire dont l'équipage fantomatique revient hanter la petite ville de Antonio Bay pour se venger ?",
+    answers: [
+      "The Sea Grace",
+      "The Elizabeth Dane",
+      "The Black Pearl",
+      "The Lady Washington",
+    ],
+    correct: "The Elizabeth Dane",
+    bg: "/fog.webp",
+    audioUrl: "/fog.mp3",
+  },
+  {
+    question:
+      "Dans La Maison des 1000 morts, quel est le nom du personnage excentrique qui tient une station-service et un musée des horreurs ?",
+    answers: [
+      "Otis Driftwood",
+      "Captain Spaulding",
+      "Baby Firefly",
+      "Rufus Firefly",
+    ],
+    correct: "Captain Spaulding",
+    bg: "/1000.webp",
+    audioUrl: "/1000.mp3",
+  },
+  {
+    question:
+      "Dans The Conjuring (2013), quels enquêteurs paranormaux célèbres sont les protagonistes du film ?",
+    answers: [
+      "Ed et Lorraine Warren",
+      "Mulder et Scully",
+      "Sam et Dean Winchester",
+      "Peter Venkman et Ray Stantz",
+    ],
+    correct: "Ed et Lorraine Warren",
+    bg: "/conjuring.webp",
+    audioUrl: "/conjuring.mp3",
+  },
+  {
+    question:
+      "Dans The Thing (1982), quel test utilise MacReady (kurt russell) pour identifier la créature extraterrestre parmi les membres de l'équipe ?",
+    answers: [
+      "Test ADN avec un microscope",
+      "Test de chaleur avec du fil chauffé",
+      "Test de sang avec un scalpel",
+      "Test d'urine sous rayons UV",
+    ],
+    correct: "Test de chaleur avec du fil chauffé",
+    bg: "/thing.webp",
+    audioUrl: "/thing.mp3",
+  },
+  {
+    question:
+      "quel est le nom du vaisseau spatial dans alien sur lequel l'équipage découvre la créature extraterrestre ?",
+    answers: ["Sulaco", "Prometheus", "Nostromo", "Covenant"],
+    correct: "Nostromo",
+    bg: "/alien.webp",
+    audioUrl: "/alien.mp3",
+  },
+  {
+    question:
+      "Dans le film Inseminoid (1981), quel est le principal danger auquel est confronté l'équipage après la rencontre avec la créature extraterrestre ?",
+    answers: [
+      "Infection virale",
+      "Possession psychique",
+      "Fécondation extraterrestre",
+      "Robot défectueux",
+    ],
+    correct: "Fécondation extraterrestre",
+    bg: "/inseminoid.webp",
+    audioUrl: "/inseminoid.mp3",
+  },
+  {
+    question:
+      "Dans le film Amityville, quel événement tragique s'est produit dans la maison avant que la famille Lutz ne s'y installe ?",
+    answers: [
+      "Un incendie",
+      "Une série de meurtres",
+      "Une disparition mystérieuse",
+      "Un suicide collectif",
+    ],
+    correct: "Une série de meurtres",
+    bg: "/amityville.webp",
+    audioUrl: "/amityville.mp3",
+  },
+  {
+    question: "De quel film est issu cette photo ?",
+    answers: ["Cop killer", "Killer murder", "Maniac cop", "Serial cop"],
+    correct: "Maniac cop",
+    bg: "/maniaccop.webp",
+    audioUrl: "/maniaccop.mp3",
+  },
+  {
+    question: "Comment peut-on invoquer Candyman dans le film ?",
+    answers: [
+      "En chantant une comptine spécifique devant un miroir",
+      "En allumant une bougie et prononçant son nom trois fois",
+      "En disant son nom cinq fois devant un miroir",
+      "En dessinant son symbole sur le mur et disant son nom à voix haute",
+    ],
+    correct: "En disant son nom cinq fois devant un miroir",
+    bg: "/candyman.webp",
+    audioUrl: "/candyman.mp3",
+  },
+  {
+    question:
+      "Dans Insidious quel est le nom du jeune garçon qui tombe dans le coma au début du film ?",
+    answers: ["Patrick Wilson", "Dalton Lambert", "Tom Rainier", "Josh dalton"],
+    correct: "Dalton Lambert",
+    bg: "/insidious.webp",
+    audioUrl: "/insidious.mp3",
+  },
+  {
+    question:
+      "Quel est le réalisateur du remake de La Colline a des Yeux de 2006 ?",
+    answers: ["Rob Zombie", "Wes Craven", "James Wan", "Alexandre Aja"],
+    correct: "Alexandre Aja",
+    bg: "/colline.webp",
+    audioUrl: "/colline.mp3",
+  },
+  {
+    question: "Quel est le nom de la famille centrale dans Hérédité ?",
+    answers: [
+      "La famille Collet",
+      "La famille Graham",
+      "La famille Paimon",
+      "La famille Satony",
+    ],
+    correct: "La famille Graham",
+    bg: "/heredite.webp",
+    audioUrl: "/heredite.mp3",
+  },
+  {
+    question:
+      "Quel est le nom de l'agent du FBI qui enquête sur Buffalo Bill dans le silence des agneaux ?",
+    answers: [
+      "Clarice Stinger",
+      "Clarice Lecter",
+      "Clarice Stenton",
+      "Clarice Starling",
+    ],
+    correct: "Clarice Starling",
+    bg: "/silence.webp",
+    audioUrl: "/silence.mp3",
+  },
+  {
+    question:
+      "Quel type de logement Tess réserve à Détroit qui se révèle problématique ?",
+    answers: [
+      "Un Airbnb",
+      "Un appartement",
+      "Une chambre d’hôte",
+      "Une maison de vacances",
+    ],
+    correct: "Un Airbnb",
+    bg: "/barbarian.webp",
+    audioUrl: "/barbarian.mp3",
+  },
+  {
+    question: "Quel est le principal antagoniste du film Phantasm de 1979 ?",
+    answers: ["The Ghoul", "The Mortuary Man", "The Tall Man", "The Demon"],
+    correct: "The Tall Man",
+    bg: "/phantasm.webp",
+    audioUrl: "/phantasm.mp3",
+  },
+  {
+    question:
+      "Quel est le nom du créateur des marionnettes dans Puppet Master ?",
+    answers: ["André Toulon", "Jester", "Blade", "Le Marseillais"],
+    correct: "André Toulon",
+    bg: "/puppet.webp",
+    audioUrl: "/puppet.mp3",
+  },
+  {
+    question: "Quel est le nom du tueur principal dans Terrifier ?",
+    answers: [
+      "Penny le clown",
+      "Tobby le clown",
+      "Art le Clown",
+      "Otis le clown",
+    ],
+    correct: "Art le Clown",
+    bg: "/terrifier.webp",
+    audioUrl: "/terrifier.mp3",
+  },
+  {
+    question:
+      "Quel est le nom de l'adolescent qui devient obsédé par sa voiture dans le film christine de john Carpenter ?",
+    answers: ["Dennis", "Arnie", "Buddy", "Lee"],
+    correct: "Arnie",
+    bg: "/christine.webp",
+    audioUrl: "/christine.mp3",
+  },
+  {
+    question: "De quel film est extrait cette musique ?",
+    answers: ["The Ring", "Les Autres", "Poltergeist", "Shining"],
+    correct: "Poltergeist",
+    bg: "/poltergeist.webp",
+    audioUrl: "/poltergeist.mp3",
+  },
+  {
+    question:
+      "Quel est le vrai nom du personnage connu sous le nom de Leatherface ?",
+    answers: ["Thomas Hewitt", "Bubba Sawyer", "Jeb Sawyer", "Charlie Hewitt"],
+    correct: "Bubba Sawyer",
+    bg: "/leatherface.webp",
+    audioUrl: "/leatherface.mp3",
+  },
 ];
--- quizz.js
+++ quizz.js
@@ -20,7 +20,14 @@ function getTitleElement(text) {
   const title = document.createElement("h3");
   title.style.color = "white";
   title.innerText = text;
-  title.style.fontSize = "20px";
+  title.classList.add(
+    "smallMobile:text-base",
+    "smallMobile:text-center",
+    "smallMobile:pt-10",
+    "tablet:text-xl",
+    "tablet:w-[90%]",
+    "largeDesktop:text-2xl"
+  );
   return title;
 }
 
@@ -52,25 +59,42 @@ function getAnswerElement(text) {
   return label;
 }
 
+//function creation poster
+function getPoster(bg) {
+  const poster = document.createElement("img");
+  poster.setAttribute("src", `${bg}`);
+  const altElement = bg.replace("/", "");
+  poster.setAttribute("alt", `poster du film ${altElement} `);
+  poster.classList.add(
+    "animate-fadeIn",
+    "delay-1000",
+    "w-[280px]",
+    "h-[300px]",
+    "object-contain",
+    "mt-10",
+    "smallMobile:mt-5",
+    "smallMobile:w-[150px]",
+    "smallMobile:h-[200px]",
+    "tablet:w-[200px]",
+    "tablet:h-[250px]",
+    "laptop:w-[250px]",
+    "laptop:h-[280px]",
+    "largeDesktop:w-[300px]",
+    "largeDesktop:h-[350px]"
+  );
+  return poster;
+}
+
 // function qui change le style du wrapper suivant la question
-function changeStyleWrapper(bg) {
-  wrapper.style.backgroundImage = `url(${bg})`;
+function changeStyleWrapper() {
   wrapper.style.height = "auto";
   wrapper.classList.add(
     "smallMobile:top-[10%]",
     "smallMobile:w-full",
-    "smallMobile:bg-none",
-    "smallMobile:bg-cover",
-    "tablet:bg-[center_left_2rem]",
-    "tablet:bg-[length:150px_200px]",
-    "largeTablet:bg-[length:150px_200px]",
-    "largeTablet:top-[3%]",
-    "laptop:bg-[center_left_3rem]",
-    "largeDesktop:bg-[length:170px_220px]",
-    "largeDesktop:bg-[center_left_2rem]",
-    "largeDesktop:bg-[length:200px_250px]",
-    "largeDesktop:w-[50%]",
-    "largeDesktop:top-[20%]"
+    "largeTablet:top-[5%]",
+    "laptop:top-[15%]",
+    "largeDesktop:w-[80%]",
+    "largeDesktop:top-[10%]"
   );
 }
 
@@ -81,19 +105,21 @@ function createAnswers(answers) {
     "flex",
     "flex-col",
     "smallMobile:w-full",
-    "tablet:items-end",
+    "smallMobile:items-center",
+    "smallMobile:mt-5",
     "tablet:w-[80%]",
-    "laptop:w-[50%]",
-    "largeDesktop:w-[100%]",
-    "tablet:mt-10"
+    "tablet:mt-10",
+    "laptop:w-[60%]"
   );
 
   for (const answer of answers) {
     const label = getAnswerElement(answer);
     label.classList.add(
-      "smallMobile:w-full",
+      "smallMobile:w-[70%]",
       "smallMobile:text-base",
-      "smallMobile:p-[8px]",
+      "smallMobile:p-[3px]",
+      "tablet:text-lg",
+      "tablet:p-2",
       "tablet:w-[250px]",
       "largeTablet:w-[350px]",
       "p-[12px]",
@@ -106,7 +132,7 @@ function createAnswers(answers) {
       "items-center",
       "justify-center",
       "text-xl",
-      "mt-5",
+      "mt-2",
       "has-[:checked]:border-tomato",
       "has-[:checked]:bg-tomato",
       "bg-white",
@@ -201,16 +227,35 @@ function displayQuestion(index) {
 
   const title = getTitleElement(question.question);
   const answersDiv = createAnswers(question.answers, question.bg);
+
+  // get poster of film
+  const poster = getPoster(question.bg);
+
+  // answers container (poster + form)
+  const answerContainer = document.createElement("div");
+  answerContainer.classList.add(
+    "flex",
+    "items-center",
+    "w-full",
+    "h-auto",
+    "smallMobile:flex-col",
+    "laptop:flex-row",
+    "laptop:justify-center",
+    "laptop:p-5"
+  );
+  answerContainer.appendChild(poster);
+  answerContainer.appendChild(answersDiv);
+
   const player = getAudioElement(question.audioUrl);
   const displayNumbersOfQuestions = getQuestionsNumbers();
 
   const validationButton = getSubmitButton();
 
-  changeStyleWrapper(question.bg);
+  changeStyleWrapper();
 
   wrapper.appendChild(displayNumbersOfQuestions);
   wrapper.appendChild(title);
-  wrapper.appendChild(answersDiv);
+  wrapper.appendChild(answerContainer);
   wrapper.appendChild(validationButton);
   wrapper.appendChild(player);
   getSubmitButton(validationButton);
@@ -226,7 +271,12 @@ function showErrorInputValidation() {
   const messageError = document.createElement("p");
   messageError.innerText = "Veuillez selectionner une réponse";
   messageError.style.color = "white";
-  messageError.classList.add("animate-fadeIn");
+  messageError.classList.add(
+    "animate-fadeIn",
+    "smallMobile:text-sm",
+    "smallMobile:pb-5",
+    "largeTablet:pb-0"
+  );
   return messageError;
 }
 
@@ -240,14 +290,15 @@ function getFeedBackMessage(isCorrect, correctAnswer) {
   message.style.width = "100%";
   message.classList.add(
     "flex",
-    "justify-end",
-    "mr-20",
+    "justify-center",
     "mt-5",
     "smallMobile:text-center",
+    "smallMobile:text-sm",
     "smallMobile:mr-0",
     "largeTablet:justify-center",
     "largeTablet:mr-0",
-    "animate-fadeIn"
+    "animate-fadeIn",
+    "largeDesktop:text-base"
   );
   return message;
 }
@@ -262,25 +313,38 @@ function startQuiz(event) {
 function displayFinishMessage() {
   const title = document.createElement("h2");
   title.innerText = "Bravo vous êtes arrivé à la fin du quiz !";
-  title.style.color = "white";
-  title.style.fontSize = "20px";
+  title.classList.add(
+    "text-white",
+    "text-xl",
+    "smallMobile:text-center",
+    "smallMobile:text-lg",
+    "smallMobile:pt-5",
+    "laptop:text-2xl",
+    "largeDesktop:text-3xl"
+  );
   const message = document.createElement("p");
   message.style.color = "white";
   message.innerText = `Votre score est de ${score} sur ${Questions.length} points...`;
-  const felicitations = document.createElement("p");
-  felicitations.innerText =
+  const commentResult = document.createElement("p");
+  commentResult.innerText =
     score <= Math.floor(Questions.length / 2)
       ? "Peu mieux faire :("
       : "Pas mal du tout :)";
-  felicitations.style.color = "white";
+  commentResult.style.color = "white";
+
+  const bestUser = document.createElement("p");
+  bestUser.innerText =
+    "Terrifiant ! Vous êtes imbattable sur le cinéma d'horreur ! 👻 ";
+  bestUser.style.color = "white";
 
   const reloadButton = getreloadButton();
 
   wrapper.appendChild(title);
   wrapper.appendChild(message);
-  wrapper.appendChild(felicitations);
+  score == Questions.length
+    ? wrapper.appendChild(bestUser)
+    : wrapper.appendChild(commentResult);
   wrapper.appendChild(reloadButton);
-  wrapper.style.backgroundImage = "none";
 
   reloadButton.addEventListener("click", () => {
     reloadPage();
@@ -291,7 +355,7 @@ function displayFinishMessage() {
 function getNextButton(index) {
   const isLastQuestion = index === Questions.length - 1;
   const nextButton = createButton(
-    isLastQuestion ? "Résultat" : "Question suivante"
+    isLastQuestion ? "Voir mon score" : "Question suivante"
   );
   return nextButton;
 }
@@ -311,7 +375,7 @@ function getreloadButton() {
   return reloadButton;
 }
 
-// Fonction pour recharger la page aprés la fin du quizhttp://localhost:5173/gladiator.webp
+// Fonction pour recharger la page aprés la fin du quiz
 function reloadPage() {
   location.reload(); // Recharge la page
 }
@@ -334,15 +398,17 @@ function createButton(text) {
     "border-none",
     "rounded-2xl",
     "mt-10",
+    "smallMobile:mt-5",
     "min-w-[250px]",
     "h-[38px]",
-    "min-[320px]:mb-5"
+    "min-[320px]:mb-5",
+    "largeTablet:mb-0",
+    "largeDesktop:text-lg"
   );
   return button;
 }
 
 // function qui affiche le nombre de questions du quiz
-
 function getQuestionsNumbers() {
   const numberOfQuestions = document.createElement("div");
   numberOfQuestions.classList.add(
@@ -350,13 +416,14 @@ function getQuestionsNumbers() {
     "justify-center",
     "items-center",
     "absolute",
-    "-top-5",
     "text-black",
     "w-[200px]",
     "h-auto",
     "bg-tomato",
     "p-2",
-    "rounded-2xl"
+    "rounded-2xl",
+    "smallMobile:-top-7",
+    "largeTablet:-top-5"
   );
   numberOfQuestions.innerText = `${currentQuestion} / ${Questions.length - 1}`;
   return numberOfQuestions;
